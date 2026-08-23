e245cbfad8856c484378487b20dfa38a9dc166ee
Nusrat Jahan Mim <njmim01627@gmail.com>
2026-08-23 11:14:30 +0600

    Refactor admin login function for clarity and flow
    Login admin [employeee] 
    
    nj

--- Admin login .c
+++ Admin login .c
@@ -1,32 +1,57 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define ADMIN_USER "admin"
 #define ADMIN_PASS "1234"
+
+void employeePanel();   // Function declaration
 
 void adminLogin()
 {
     char username[30];
     char password[20];
+    int attempt = 0;
 
-    printf("\n[ADMIN LOGIN]\n");
-
-    printf("Username: ");
-    scanf("%29s", username);
-
-    printf("Password: ");
-    scanf("%19s", password);
-
-    if(strcmp(username, ADMIN_USER) == 0 &&
-       strcmp(password, ADMIN_PASS) == 0)
+    while(attempt < 3)
     {
-        printf("\nLogin Successful!\n");
-        printf("Welcom to NUB Canteen\n");
+        printf("\n[ADMIN LOGIN]\n");
+
+        printf("Username: ");
+        scanf("%29s", username);
+
+        printf("Password: ");
+        scanf("%19s", password);
+
+        if(strcmp(username, ADMIN_USER) == 0 &&
+           strcmp(password, ADMIN_PASS) == 0)
+        {
+            printf("\nLogin Successful!\n");
+            printf("Welcome to NUB Canteen\n");
+
+            employeePanel();
+
+            return;
+        }
+        else
+        {
+            attempt++;
+
+            printf("\nWrong Username or Password\n");
+            printf("Remaining Attempt: %d\n", 3 - attempt);
+        }
     }
-    else
-    {
-        printf("\nInvalid Username or Password! Try again\n");
-    }
+
+    printf("\nAccess Denied.\n");
+    system("pause");
+}
+
+void employeePanel()
+{
+    printf("\n===== EMPLOYEE PANEL =====\n");
+    printf("Welcome Employee!\n");
+
+    system("pause");
 }
 
 int main()
