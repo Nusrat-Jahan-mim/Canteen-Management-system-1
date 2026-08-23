3f8d2f17d6dfb24ebb41500dbb78590e54b01e15
Nusrat Jahan Mim <njmim01627@gmail.com>
2026-08-23 08:31:46 +0600

    Add header file with function declarations and macros
    nj

--- Functions.h
+++ Functions.h
@@ -1,1 +1,28 @@
 
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#define ADMIN_USER "admin"
+#define ADMIN_PASS "1234"
+
+#define EMP_USER "employee"
+#define EMP_PASS "1234"
+
+void loading();
+void mainMenu();
+void adminLogin();
+void employeeLogin();
+void adminPanel();
+void employeePanel();
+
+#endif
+
+
+void employeeLogin();
+void employeePanel();
+
+
