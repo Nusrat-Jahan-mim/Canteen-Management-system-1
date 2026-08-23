ce1d83b535b5f6698ed09d1df090402e79350a48
Nusrat Jahan Mim <njmim01627@gmail.com>
2026-08-23 08:25:39 +0600

    Implement loading and main menu functions
    Added loading function and main menu for user interaction.
    
    nj

--- Functions.c
+++ Functions.c
@@ -22,3 +22,65 @@ void employeePanel();
 #endif
 
 
+#include "functions.h"
+
+void loading()
+{
+    int i;
+
+    printf("\n\n");
+    printf("=====================================\n");
+    printf("         NUB CANTEEN SYSTEM\n");
+    printf("=====================================\n");
+
+    printf("\nLoading");
+
+    for(i=0;i<8;i++)
+    {
+        printf(".");
+        fflush(stdout);
+
+        #ifdef _WIN32
+        Sleep(250);
+        #endif
+    }
+
+    printf("\n\n");
+}
+
+void mainMenu()
+{
+    int choice;
+
+    while(1)
+    {
+        printf("\n=============================\n");
+        printf("      MAIN MENU\n");
+        printf("=============================\n");
+
+        printf("1. Admin Login\n");
+        printf("2. Employee Login\n");
+        printf("3. Exit\n");
+
+        printf("\nEnter Choice : ");
+        scanf("%d",&choice);
+
+        switch(choice)
+        {
+            case 1:
+                adminLogin();
+                break;
+
+            case 2:
+                employeeLogin();
+                break;
+
+            case 3:
+                printf("\nThank You.\n");
+                exit(0);
+
+            default:
+                printf("\nInvalid Choice\n");
+        }
+    }
+}
